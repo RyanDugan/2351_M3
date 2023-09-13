0898077e5bf27873fc8f563fa5bfc125db8fa519
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-13 13:04:52 -0500

    added content to question

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 let randomQuestion =
-  "Would you rather see something from the past or from the future?";
+  "Would you rather see something/someone from the past or from the future?";
 console.log(hello);
