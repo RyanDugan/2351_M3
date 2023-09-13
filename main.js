362ec26ff4e051ba5cd043a0a7bc58264a97f4ba
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-13 13:19:59 -0500

    added math function

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 let randomQuestion =
   "Would you rather see something/someone from the past or from the future?";
 console.log(randomQuestion);
+
+let x = mathFunction(4, 8);
+document.getElementById("test1").innerHTML = x;
+
+function mathFunction(x, y) {
+  //the x & y properties translate to the 4 & 8 properties
+  return x + y + " is the result of 8 + 4";
+}
+
+console.log(x);
