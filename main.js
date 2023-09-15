a915a92d6db0c4328b4d874a7f6e9a567edfa3cd
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-15 11:10:54 -0500

    added content to button event

--- main.js
+++ main.js
@@ -39,4 +39,5 @@ document.getElementById("test4").innerHTML =
 function clickFunction() {
   document.getElementById("btnTest").innerHTML = "You clicked me!";
   document.getElementById("test5").innerHTML = "Testing Complete!";
+  console.log("Nice!");
 }
